2ed3a98b879f833caa51ff7e1385790b283df965
Neil Macneale V <neil.macneale.v@gmail.com>
2024-04-02 17:02:16 -0700

    Fix goto next/prev

--- init.lua
+++ init.lua
@@ -167,8 +167,8 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
@@ -589,11 +589,11 @@ require('lazy').setup({
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           map('[g', function()
-            vim.diagnostic.goto_prev { wrap = false }
+            vim.diagnostic.goto_prev()
           end, 'Goto previous diagnostic')
 
           map(']g', function()
-            vim.diagnostic.goto_next { wrap = false }
+            vim.diagnostic.goto_next()
           end, 'Goto next diagnostic')
 
           -- The following two autocommands are used to highlight references of the
