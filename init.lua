96cdd122dbf5b183039ccf30504c3a07757db900
Neil Macneale V <neil.macneale.v@gmail.com>
2025-01-22 10:52:57 -0800

    Add jdtls

--- init.lua
+++ init.lua
@@ -1113,11 +1113,36 @@ require('lazy').setup({
 
   {
     'mfussenegger/nvim-jdtls',
-    ft = { 'scala', 'sbt' },
-    opts = function()
+    ft = { 'java' },
+    config = function()
       local config = {
+        cmd = {
+          '/usr/lib/jvm/java-21-openjdk/bin/java',
+
+          '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+          '-Dosgi.bundles.defaultStartLevel=4',
+          '-Declipse.product=org.eclipse.jdt.ls.core.product',
+          '-Dlog.protocol=true',
+          '-Dlog.level=ALL',
+          '-Xmx1g',
+          '--add-modules=ALL-SYSTEM',
+          '--add-opens',
+          'java.base/java.util=ALL-UNNAMED',
+          '--add-opens',
+          'java.base/java.lang=ALL-UNNAMED',
+
+          '-jar',
+          '/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
+
+          '-configuration',
+          '/home/macmv/.local/share/jdtls/config_linux',
+
+          '-data',
+          vim.fn.stdpath 'data' .. '/jdtls-workspace',
+        },
         root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git' }, { upward = true })[1]),
       }
+
       require('jdtls').start_or_attach(config)
     end,
   },
