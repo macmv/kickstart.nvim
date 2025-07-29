9695734e85f95906372aa7c21a57ec2d28e7c223
Neil Macneale V <neil.macneale.v@gmail.com>
2025-07-28 22:11:17 -0700

    Add lush

--- init.lua
+++ init.lua
@@ -970,6 +970,10 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'rktjmp/lush.nvim',
+  },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
